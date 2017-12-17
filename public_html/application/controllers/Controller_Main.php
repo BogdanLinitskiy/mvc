5dc3649109a1f08ebbe081e86123ed74e29ab78a
John Doe <johndoe@gmail.com>
2017-12-17 17:02:57 +0200

    added 2 Controllers:Blog and main, each one got 2 methods

---
+++ public_html/application/controllers/Controller_Main.php
@@ -0,0 +1,13 @@
+<?php
+
+class Controller_Main
+{
+	public function action_index(){
+		echo 'Main page';
+		die();
+	}
+	public function action_contacts(){
+		echo 'Contacts page';
+		die();
+	}
+}
--- public_html/application/core/Route.php
+++ public_html/application/core/Route.php
@@ -5,6 +5,7 @@ class Route
 {
 	static function start()
 	{
+		//mysite.dev/segment_1/segment_2
 		$segments = explode('/', $_SERVER['REQUEST_URI']);
 
 		$controllerName = 'Main';
@@ -12,14 +13,32 @@ class Route
 
 		if(!empty($segments[1])){
 			$controllerName = $segments[1];
+			if(!empty($segments[2])){
+				$actionName = $segments[2];
+			}
 		}
 
-		if(!empty($segments[2])){
-			$actionName = $segments[2];
-		}
-		$controllerFile = 'Controller'.$controllerName;
+		$controllerFile = 'Controller_'.$controllerName;
 		$actionName = 'action_'.$actionName;
+
+		$controllerPath = 'application/controllers/' . $controllerFile . '.php';
+		if(file_exists($controllerPath)){
+			require_once $controllerPath;
+		}else{
+			Route::errorPage404();
+		}
+		$controller = new $controllerFile();
+		if(method_exists($controller,$actionName)){
+			$controller->$actionName();
+		}else{
+			Route::errorPage404();
+		}
+	}
+
+	static function errorPage404(){
+		echo "sorry, page ot found";
+		die();
 	}
 }
-//explode превращает страку в массив
+//explode превращает строку в массив
 // 1arg - разделитель,2 - строка
